0e639c1ce56b43df6d9240fd8f9a49e44a38985d
masasami <masangular0518@gmail.com>
2021-10-31 00:48:04 +0900

    アクセスを許可するオリジンを設定

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+load_dotenv()
 
 # ルーター
 from routers.connect_router import connect_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv('ALLOW_ORIGIN')],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(connect_router)
 
